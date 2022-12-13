b4f906b637bb1c667aedbcf4f88767749dc773ce
rafael rubenstein <73216681+rafaelrubenstein@users.noreply.github.com>
2022-12-12 23:00:05 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+if __name__ =='__main__':
+    app.run()
